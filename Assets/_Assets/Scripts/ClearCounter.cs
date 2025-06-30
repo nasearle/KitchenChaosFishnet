8c4f0611c1afa330f673b18d998ebdc5be73eabd
nasearle <nicolasasearle@gmail.com>
2025-06-30 14:54:21 -0700

    Adds ability to pick up and drop items
    - adds a few container counter prefab variants for the different objects

--- Assets/_Assets/Scripts/ClearCounter.cs
+++ Assets/_Assets/Scripts/ClearCounter.cs
@@ -5,6 +5,14 @@ public class ClearCounter : BaseCounter {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
     public override void Interact(Player player) {
-        
+        if (!HasKitchenObject()) {
+            if (player.HasKitchenObject()) {
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+        } else {
+            if (!player.HasKitchenObject()) {
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
     }
 }
--- Assets/_Assets/Scripts/ContainerCounter.cs
+++ Assets/_Assets/Scripts/ContainerCounter.cs
@@ -5,9 +5,11 @@ public class ContainerCounter : BaseCounter {
     public event EventHandler OnPlayerGrabbedObject;
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
     public override void Interact(Player player) {
-        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
-        kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
-        
-        OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
+        if (!player.HasKitchenObject()) {
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
+
+            OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
+        }
     }
 }
