93c223c84d9c0074bca91e1d4744a72980f873c2
nasearle <nicolasasearle@gmail.com>
2025-07-11 13:37:32 -0700

    Adds key rebinding in the options menu

--- Assets/_Assets/Scripts/GameInput.cs
+++ Assets/_Assets/Scripts/GameInput.cs
@@ -3,11 +3,22 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class GameInput : MonoBehaviour {
+    private const string PLAYER_INPUT_BINDINGS = "InputBindings";
     public static GameInput Instance { get; private set; }
     
     public event EventHandler OnInteractAction;
     public event EventHandler OnInteractAlternateAction;
     public event EventHandler OnPauseAction;
+
+    public enum Binding {
+        Move_Up,
+        Move_Down,
+        Move_Left,
+        Move_Right,
+        Interact,
+        InteractAlternate,
+        Pause
+    }
     
     private PlayerInputActions _playerInputActions;
     
@@ -15,6 +26,11 @@ public class GameInput : MonoBehaviour {
         Instance = this;
         
         _playerInputActions = new PlayerInputActions();
+
+        if (PlayerPrefs.HasKey(PLAYER_INPUT_BINDINGS)) {
+            _playerInputActions.LoadBindingOverridesFromJson(PlayerPrefs.GetString(PLAYER_INPUT_BINDINGS));
+        }
+        
         _playerInputActions.Player.Enable();
         
         _playerInputActions.Player.Interact.performed += InteractOnperformed;
@@ -46,5 +62,80 @@ public class GameInput : MonoBehaviour {
         Vector2 inputVector = _playerInputActions.Player.Move.ReadValue<Vector2>();
         inputVector.Normalize();
         return inputVector;
+    }
+
+    public string GetBindingText(Binding binding) {
+        switch (binding) {
+            case Binding.Move_Up:
+                // WASD composite up (index 1)
+                return _playerInputActions.Player.Move.bindings[1].ToDisplayString();
+            case Binding.Move_Down:
+                // WASD composite down (index 2)
+                return _playerInputActions.Player.Move.bindings[2].ToDisplayString();
+            case Binding.Move_Left:
+                // WASD composite left (index 3)
+                return _playerInputActions.Player.Move.bindings[3].ToDisplayString();
+            case Binding.Move_Right:
+                // WASD composite right (index 4)
+                return _playerInputActions.Player.Move.bindings[4].ToDisplayString();
+            case Binding.Interact:
+                return _playerInputActions.Player.Interact.bindings[0].ToDisplayString();
+            case Binding.InteractAlternate:
+                return _playerInputActions.Player.InteractAlternate.bindings[0].ToDisplayString();
+            case Binding.Pause:
+                return _playerInputActions.Player.Pause.bindings[0].ToDisplayString();
+            default:
+                return "";
+        }
+    }
+
+    public void RebindBinding(Binding binding, Action onActionRebound) {
+        _playerInputActions.Player.Disable();
+
+        InputAction inputAction;
+        int bindingIndex;
+
+        switch (binding) {
+            default:
+            case Binding.Move_Up:
+                inputAction = _playerInputActions.Player.Move;
+                bindingIndex = 1;
+                break;
+            case Binding.Move_Down:
+                inputAction = _playerInputActions.Player.Move;
+                bindingIndex = 2;
+                break;
+            case Binding.Move_Left:
+                inputAction = _playerInputActions.Player.Move;
+                bindingIndex = 3;
+                break;
+            case Binding.Move_Right:
+                inputAction = _playerInputActions.Player.Move;
+                bindingIndex = 4;
+                break;
+            case Binding.Interact:
+                inputAction = _playerInputActions.Player.Interact;
+                bindingIndex = 0;
+                break;
+            case Binding.InteractAlternate:
+                inputAction = _playerInputActions.Player.InteractAlternate;
+                bindingIndex = 0;
+                break;
+            case Binding.Pause:
+                inputAction = _playerInputActions.Player.Pause;
+                bindingIndex = 0;
+                break;
+        }
+
+        inputAction.PerformInteractiveRebinding(bindingIndex)
+            .OnComplete(callback => {
+                callback.Dispose();
+                _playerInputActions.Player.Enable();
+                onActionRebound();
+
+                PlayerPrefs.SetString(PLAYER_INPUT_BINDINGS, _playerInputActions.SaveBindingOverridesAsJson());
+                PlayerPrefs.Save();
+            })
+            .Start();
     }
 }
